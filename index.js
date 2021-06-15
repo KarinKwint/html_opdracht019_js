c677d07b5eb713a550f3522e608249de3933249d
KarinKwint <83405911+KarinKwint@users.noreply.github.com>
2021-06-15 16:40:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+//Part 1 true false
+const add = function (number1, number2) {
+    const sum = number1 + number2;
+    return sum;
+};
+add(33, 98);
+
+const result = add(33, 98)
+
+if (result > 100) {
+    console.log("true");
+}
+else {
+    console.log("false")
+}
+
+//Part 2 call function with different numbers
+const add = function (number1, number2) {
+    const sum = number1 + number2;
+    return sum;
+};
+
+add(33, 98);
+
+const result = add(33, 98)
+console.log(result);
+
+//Bouncer at a club
+const bouncerBrenda = function (action) {
+    console.log(action);
+};
+
+const age = 27;
+const currentPeople = 98;
+const maxPeople = 100;
+
+if (age >= 18 && currentPeople >= 100); {
+    console.log('Come in');
+} if (currentPeople >= 100); {
+    console.log('it is too busy now, come back later');
+} if (age <= 18); {
+    console.log('this is a club for adults');
+};
+
+//Calculating the average
+const averageCalculate = function (numbers) {
+    console.log(grades)
+    Math.round;
+};
+
+const grades = [12, 36, 88, 13, 45]
+averageCalculate(grades);
